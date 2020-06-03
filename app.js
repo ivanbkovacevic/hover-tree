f00e2ebca5d6ac611f9bf3d7aac3d6a79c34810c
unknown <ivanbkovacevic@gmail.com>
2020-06-03 17:44:47 +0200

    jfebv

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 new hoverEffect({
 parent: document.querySelector('.distortion'),
 intensity:0.2,
-image1:'./maja1.jpg',
-image2:'./maja2.jpg',
+image1:'./beach1.jpg',
+image2:'./beach2.jpg',
 displacementImage:'./fluid.jpg'
 });

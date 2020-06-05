a75833d037ccbf5ebd3619720471962831c0d1bc
ivanbkovacevic <ivanbkovacevic@gmail.com>
2020-06-06 00:10:17 +0200

    hb

--- myAnime.js
+++ myAnime.js
@@ -24,26 +24,47 @@ function gigleStop(){
 }
 
 let kvadNum;
-   function launching(){
-    let min = 1;
-    let max = 30;
-    kvadNum=Math.floor(Math.random() * (max - min + 1)) + min;
-    console.log(kvadNum)
-    var objPropLogEl = document.querySelector(`.spreading-kvad:nth-of-type(${kvadNum})`);
+let niz=[];
+let min = 1;
+let max = 30;
+let clickOrd=0;
 
+let temp
+  
+for(let i=0; i<30;i++){
+    niz.push(i);
+  }
+
+  for (var j = niz.length - 1; j > 0; j--) {
+    kvadNum=Math.floor(Math.random() * (max - min + 1)) + min;
+     
+         temp = niz[j];
+        niz[j] = niz[kvadNum];
+        niz[kvadNum] = temp;
+  }
+
+   function launching(){
+      
+    console.log(kvadNum)
+    console.log(temp)
+    let kvadNumstring=JSON.stringify(temp);
+    var objPropLogEl = document.querySelector(`.spreading-kvad:nth-of-type(${temp})`);
+     clickOrd++;
     console.log(objPropLogEl);
   
     anime({
         targets: objPropLogEl,
         translateX: 250,
         translateY:200,
-        duration: 1200,
+        duration: 400,
         easing: 'easeInOutQuad',
         update: function() {
            objPropLogEl.innerHTML = JSON.stringify(kvadNum);
           }//update
     })
    }
+
+
 
 
 

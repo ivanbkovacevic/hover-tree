17c15b07e0c8ec8c987333680680ac52ef2aa6fc
ivanbkovacevic <ivanbkovacevic@gmail.com>
2020-06-06 22:51:33 +0200

    povecan broj

--- myAnime.js
+++ myAnime.js
@@ -11,14 +11,15 @@ onSelectURL=(e)=>{
 let kvadNum;
 let kvadNumArr=[];
 let min = 1;
-let max = 30;
+let max = 49;
 let clickOrd=-1;
 let transFX=-180;
 let transX=0;
 let transY=0;
-let kvadSize=30;
-let kvadNumberOf=30;
-let kvadCont= document.querySelector('.testsquare');
+let kvadSize=50;
+let kvadNumberOf=49;
+let kvadOneSide=7;
+let kvadCont= document.querySelector('.kvadShuff');
 let kvadOriginalArr=[];
 let kvadObj={};
 
@@ -32,13 +33,19 @@ let kvadRetOrd=0;
 let kvadFinalArr=[];
 
 let cln;
-for(let i=0; i<6;i++){
-  for(let k=0; k<6; k++){
+
+kvadCont.style.width=(`${kvadSize*kvadOneSide+10}px`);
+
+kvadCont.style.height=(`${kvadSize*kvadOneSide+10}px`);
+
+for(let i=0; i<kvadOneSide;i++){
+  for(let k=0; k<kvadOneSide; k++){
     kvadPos++
     let kvad=document.createElement("div");
-    kvadObj={position:kvadPos,height:`${kvadSize}px`,width:`${kvadSize}px` ,bcgImage:`url(${img})`,
-    bcgPosition:`${kvadSize*k*-1}px ${kvadSize*i*-1}px`, zIndex:`99`,transition:`all 0.8s`,xpoz:k*30,ypoz:i*30,
-    bcgSize:`180px 180px`
+    kvadObj={position:kvadPos,height:`${kvadSize}px`,width:`${kvadSize}px` ,bcgImage:`url(beach1.jpg)`,
+    bcgPosition:`${kvadSize*k*-1}px ${kvadSize*i*-1}px`, zIndex:`99`,transition:`all 1.5s`,
+    xpoz:k*kvadNumberOf,ypoz:i*kvadNumberOf,
+    bcgSize:`${kvadOneSide*kvadSize}px ${kvadOneSide*kvadSize}px`
   };
 
   
@@ -79,7 +86,7 @@ function backToOrder(){
           
           kvadFinal.style.transform=(`translateX:${kvadOriginalArr[i].transX*-1}`);
           kvadFinal.style.transform=(`translateY:${kvadOriginalArr[i].transY*-1}`);
-          kvadFinal.style.transition=('all 0.8s');
+          kvadFinal.style.transition=('all 1.5s');
         
           kvadFinalArr.push(kvadFinal);
 
